d82a8d3ac818a4b866f695106b9c84c368b9bd7e
Dmitriy <webcreat7@gmail.com>
2020-05-19 00:27:34 +0300

    lesson 67 Thunk function () => (dispatch) => {}

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,6 +1,6 @@
+import {authAPI} from "../api/api";
+
 const SET_USER_DATA = 'SET_USER_DATA';
-
-
 
 let initialState = {
     userId: null,
@@ -27,6 +27,16 @@ const authReducer = (state = initialState, action) => {
 
 /* Action Creators */
 export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: {userId, email, login } });
+export const getAuthUserData = () => (dispatch) => {
+    authAPI.me()
+        .then(response => {
+            if ( response.data.resultCode === 0 ){
+                // Destructuring assignment (Destructurization) will create 3 vars userId, login, email
+                let { id, email, login } = response.data.data;
+                dispatch( setAuthUserData( id, email, login ) );
+            }
+        });
+};
 
 export default authReducer;
 
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,4 +1,6 @@
 /* Action Types, or action types constants*/
+import {usersAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE;'
@@ -54,6 +56,12 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile: profile});
+export const getUserProfile = (userId) => (dispatch) => {
+    usersAPI.getProfile(userId)
+        .then(response => {
+            dispatch( setUserProfile(response.data) );
+        });
+};
 
 export default profileReducer;
 
