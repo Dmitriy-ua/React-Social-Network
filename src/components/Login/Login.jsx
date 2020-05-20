1d85db6f5b7f4afa48a7826b9a276294b4aa4c8a
Dmitriy <webcreat7@gmail.com>
2020-05-20 11:29:23 +0300

    lesson 75 React Form yarn add redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
-const Login = () => {
-    return <h1>Login</h1>
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <p><Field component={'input'} name={'login'} placeholder={'Login'}/></p>
+        <p><Field component={'input'} name={'password'} placeholder={'Password'}/></p>
+        <p>
+            <label htmlFor="">
+                <Field component={'input'} name={'rememberMe'} type="checkbox" /> Remember me
+            </label>
+        </p>
+        <p> <button>Login</button></p>
+    </form>
 }
 
+const LoginReduxForm = reduxForm( { form: 'login' }) (LoginForm)
+
+const Login = () => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+}
 export default Login;
